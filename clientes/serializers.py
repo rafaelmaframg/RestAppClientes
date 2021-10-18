be32e40ff80414236bdb1b9b2ab3bda6cabd19c0
Rafael Mafra <rafaelmafr4@hotmail.com>
2021-10-18 22:19:02 +0100

    first commit

---
+++ clientes/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from clientes.models import Cliente
+from clientes.validators import *
+
+
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = '__all__'
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf':"Numero de Cpf Invalido"})
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome':"Não inclua números neste campo"})
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg':"O RG deve ter 9 dígitos"})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular':'O Celular deve seguir este padrao 11 92134-1234'})
+
+        return data
